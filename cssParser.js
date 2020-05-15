d15702eb256e787be0d8a0de148b9de4998fa574
Anas Fakhfakh <anasfakhfakh123@gmail.com>
2020-05-15 17:43:39 +0100

    Parser's core

--- cssParser.js
+++ cssParser.js
@@ -1,6 +1,21 @@
-//const preprocessor  = require('./preprocessor.js');
 const parser        = require('./src/parser.js');
 
 exports.parse = function(input) {
-    parser.parse(input);
+    return parser.parse(input);
+};
+
+exports.getAllDeclarationsAtBlock = function(handle, blockNumber) {
+    if(blockNumber <= Object.keys(handle).length) {
+        let numberOfDeclarations = Object.keys(handle[blockNumber.toString()]).length - 4;
+        let returnedObject = new Array();
+        for(i = 0; i < numberOfDeclarations; i++) {
+            let declarationName = "Declaration " + `${i}`;
+            returnedObject.push(handle[blockNumber.toString()][declarationName]);
+        }
+        return returnedObject;
+    }
+};
+
+exports.getTokenAtPos = function(handle, position) {
+    
 };
