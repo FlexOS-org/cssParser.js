e9f5fba185931093c9f2af2c104f04551cc6bd1b
Anas Fakhfakh <anasfakhfakh123@gmail.com>
2020-05-23 21:36:21 +0100

    Parser's core. (HIDDEN)

--- cssParser.js
+++ cssParser.js
@@ -1,4 +1,5 @@
-const parser        = require('./src/parser.js');
+const parser = require('./src/parser.js');
+const scrape = require('./src/scrape.js');
 
 exports.parse = function(input) {
     return parser.parse(input);
@@ -16,6 +17,27 @@ exports.getAllDeclarationsAtBlock = function(handle, blockNumber) {
     }
 };
 
-exports.getTokenAtPos = function(handle, position) {
+exports.preprocess = function(input) {
+    let matched     = [];
+    let matchedPos  = [];
+    if(input.search(/(@import\s*((".*")|('.*'));)|(@import\s*url\(\s*((".*")|('.*'))\s*\);)|(@import\s*url\(\s*.*\s*\);)/g) != -1) {
+        for(i = 0; i < input.match(/(@import\s*((".*")|('.*'));)|(@import\s*url\(\s*((".*")|('.*'))\s*\);)|(@import\s*url\(\s*.*\s*\);)/g).length; i++) {
+            matched.push(input.match(/(@import\s*((".*")|('.*'));)|(@import\s*url\(\s*((".*")|('.*'))\s*\);)|(@import\s*url\(\s*.*\s*\);)/g)[i]);
+            matchedPos.push()
+        }
+    }
     
-};
+    for(i = 0; i < matched.length; i++) {
+        matched[i] = matched[i].replace(/^@import\s*/g, "");
+        matched[i] = matched[i].replace(/(\s*;)$/gm, "");
+        if(matched[i].search(/(^(url\())/g) != -1) {
+            matched[i] = matched[i].replace(/(^(url\())/g, "");
+        }
+        
+        matched[i] = matched[i].replace(/(^")|(^')/g, "").replace(/("$)|('$)|('\s*\)\s*$)|("\s*\)\s*$)|(\)\s*$)/g, "");
+        
+        if(matched[i].search(/(^((https|https):\/\/www\..*\.[A-z]*))|(^((https|https):\/\/.*\.[A-z]*))|(^(www\..*\.[A-z]*))/g) != -1) {
+            return scrape.scrape(matched[i]);
+        }
+    }
+}
